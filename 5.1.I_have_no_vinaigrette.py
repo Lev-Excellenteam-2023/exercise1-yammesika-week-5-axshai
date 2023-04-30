45ab17eb0731cb463f5b50b1e76a4d06bcb6d269
Shai Axelrod <Saxelrod@g.jct.ac.il>
2023-04-30 11:18:52 +0300

    fix 5.1.I_have_no_vinaigrette

--- 5.1.I_have_no_vinaigrette.py
+++ 5.1.I_have_no_vinaigrette.py
@@ -10,7 +10,7 @@ def pick_random_date_in_range(first_date, last_date):
     :param last_date: The last date in the range.
     :return: A random date between first_date and last_date.
     """
-    first_date, last_date = (first_date, last_date) if first_date <= last_date else (last_date, first_date)
+    first_date, last_date = min(first_date, last_date), max(first_date, last_date)
     random_epoch_time = random.choice(numpy.arange(first_date.timestamp(), last_date.timestamp()))
     return datetime.datetime.fromtimestamp(random_epoch_time)
 
@@ -19,10 +19,10 @@ def main():
     """
     Gets two dates from the user and prints a message if the random date is a Monday.
     """
-    date_1_str, date_2_str = input("Enter 2 dates in the following format: YYYY-MM-DD\n").split()
-    date_1 = datetime.datetime.strptime(date_1_str, '%Y-%m-%d')
-    date_2 = datetime.datetime.strptime(date_2_str, '%Y-%m-%d')
-    random_date = pick_random_date_in_range(date_1, date_2)
+    first_date, last_date = input("Enter 2 dates in the following format: YYYY-MM-DD\n").split()
+    first_date = datetime.datetime.strptime(first_date, '%Y-%m-%d')
+    last_date = datetime.datetime.strptime(last_date, '%Y-%m-%d')
+    random_date = pick_random_date_in_range(first_date, last_date)
     if random_date.weekday() == 0:
         print("I have no vinaigrette!")
 
